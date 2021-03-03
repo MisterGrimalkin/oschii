5c03cd439033a7ad043ed43ad7dafec8971089d4
MisterGrimalkin <barri@wi5.io>
2021-03-03 16:41:59 +0000

    Add scaffold for Driver and DriverRack

--- libraries/Oschii/DriverRack.cpp
+++ libraries/Oschii/DriverRack.cpp
@@ -1,11 +1,39 @@
 #include "DriverRack.h"
 
 String DriverRack::buildDrivers(JsonArray array) {
-  return "";
-}
+  Serial.println("Building Drivers:");
+
+  String errorBuffer = "";
+  for ( int i = 0; i < array.size(); i++ ) {
+    JsonObject json = array[i];
+    errorBuffer += buildDriver(json);
+  }
+
+  Serial.print("Found: ");
+  Serial.print(_driverIndex);
+  Serial.println("\n");
+
+  return errorBuffer;}
 
 String DriverRack::buildDriver(JsonObject json) {
-  return "";
+  Driver * driver;
+
+  String type = json["type"];
+
+  if ( false ) {
+
+  } else {
+    return "RuleTwoError: No type of driver called '" + type + "'\n";
+  }
+
+  if ( driver->build(json) ) {
+    _drivers[_driverIndex++] = driver;
+
+    Serial.println(" " + driver->toString());
+    return "";
+  } else {
+    return driver->getError() + "\n";
+  }
 }
 
 StaticJsonDocument<4096> doc3;
